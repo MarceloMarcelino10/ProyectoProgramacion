abcefa17a0f50817d164657c0238a07a50b3cf2c
marce <marce@LAPTOP-95MNOIER>
2023-11-25 20:52:56 -0400

    Ahora los medicos conocen los pacientes y consultas tratadas

--- ProyectoFinalClinica/src/logico/Doctor.java
+++ ProyectoFinalClinica/src/logico/Doctor.java
@@ -1,15 +1,21 @@
 package logico;
 
+import java.util.ArrayList;
+
 public class Doctor extends Persona {
 	
 	private String especialidad;
 	boolean enServicio = false;
+	private ArrayList<Paciente> pacientesTratados;
+	private ArrayList<Consulta> consultasTratadas;
 	
 	public Doctor(String codigo, String cedula, String nombre, String apellidos, String genero, String user,
 			String password, String especialidad, boolean enServicio) {
 		super(codigo, cedula, nombre, apellidos, genero, user, password);
 		this.especialidad = especialidad;
 		this.enServicio = enServicio;
+		this.pacientesTratados = new ArrayList<Paciente>();
+		this.consultasTratadas = new ArrayList<Consulta>();
 	}
 
 	public String getEspecialidad() {
@@ -26,5 +32,69 @@ public class Doctor extends Persona {
 
 	public void setEnServicio(boolean enServicio) {
 		this.enServicio = enServicio;
+	}
+	
+	//METODOS PARA INSERTAR:
+	
+	public void insertarPacienteTratado(Paciente paciente) {
+		pacientesTratados.add(paciente);
+	}
+	
+	public void insertarConsultaTratada(Consulta consulta) {
+		consultasTratadas.add(consulta);
+	}
+	
+	//METODOS PARA ELIMINAR:
+	
+	public void eliminarPacienteTratado(Paciente paciente) {
+	    pacientesTratados.remove(paciente);
+	}
+
+	public void eliminarConsultaTratada(Consulta consulta) {
+	    consultasTratadas.remove(consulta);
+	}
+
+	// METODOS PARA MODIFICAR:
+
+	public void actualizarPacienteTratado(Paciente paciente) {
+	    int index = buscarPacienteTratadoByCodigo(paciente.getCodigo());
+	    pacientesTratados.set(index, paciente);
+	}
+	
+	private int buscarPacienteTratadoByCodigo(String codigo) {
+	    int index = -1;
+	    boolean encontrado = false;
+	    int i = 0;
+
+	    while (!encontrado && i < pacientesTratados.size()) {
+	        if (pacientesTratados.get(i).getCodigo().equalsIgnoreCase(codigo)) {
+	            encontrado = true;
+	            index = i;
+	        }
+	        i++;
+	    }
+
+	    return index;
+	}
+
+	public void actualizarConsultaTratada(Consulta consulta) {
+	    int index = buscarConsultaTratadaByCodigo(consulta.getCodigo());
+	    consultasTratadas.set(index, consulta);
+	}
+	
+	private int buscarConsultaTratadaByCodigo(String codigo) {
+	    int index = -1;
+	    boolean encontrado = false;
+	    int i = 0;
+
+	    while (!encontrado && i < consultasTratadas.size()) {
+	        if (consultasTratadas.get(i).getCodigo().equalsIgnoreCase(codigo)) {
+	            encontrado = true;
+	            index = i;
+	        }
+	        i++;
+	    }
+
+	    return index;
 	}
 }
